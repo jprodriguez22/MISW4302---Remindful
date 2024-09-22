1821530f010946de7fc51a1d6449def5a7db9e6b
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-09-21 19:04:23 -0500

    Vista principal casi terminada, faltan acciones en switches

---
+++ Remindful/app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { Surface, Icon } from "react-native-paper";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#8F75FF",        
+        tabBarStyle: { backgroundColor: "#282C35", borderColor: "#282C35"},
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Alarmas",
+          tabBarIcon: ({ color, focused }) => (
+            <Icon source="home" size={40} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
+}
