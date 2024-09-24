548ab36897ffe175c5e4a3770f020eaac6ea9c0c
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-09-23 21:59:57 -0500

    Construcción de vista principal de alarmas

---
+++ RemindfulWeb/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-col>
+      <router-view />
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+//
+</script>
+
+<style>
+body {
+  background-color: #2E3440;
+}
+p {
+  color: white;
+}
+.clock-text {
+  font-size: 96px;
+}
+.title-text {
+  font-size: 34px;
+}
+.body-text {
+  font-size: 24px;
+}
+.subtitle-text {
+  font-size: 20px;
+}
+</style>
